cfdfac2e7859e706990ffdcb384238db2394d3fd
Andres Contreras <andres_c8@outlook.com>
2021-01-20 22:23:10 -0500

    adding redux to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
+import Header from './components/Header';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Redux</h1>
-    </div>
+    <Router>
+      <Provider store={store}>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route axact path="/products/new" component={NewProduct} />
+          <Route axact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+        </div>
+        </Provider>
+    </Router>
   );
 }
 
---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,16 @@
+const initalState = {
+    products: [],
+    error: false,
+    loading: false
+}
+
+export default function (state = initalState, action) {
+    switch (action.type) {
+        case 'aa':
+            
+            break;
+    
+        default:
+            return state
+    }
+}
